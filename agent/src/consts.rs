0865f9093f5eba9b762abc6b4cbbbdd43fdab132
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-11-25 21:14:52 -0300

    update server url

--- agent/src/consts.rs
+++ agent/src/consts.rs
@@ -1,2 +1,2 @@
-pub const SERVER_URL: &str = "http://192.168.60.1:8080";
+pub const SERVER_URL: &str = "http://192.168.60.22:8080";
 pub const AGENT_ID_FILE: &str = "ch_10";
--- client/src/config.rs
+++ client/src/config.rs
@@ -1,1 +1,1 @@
-pub const SERVER_URL: &str = "http://192.168.60.1:8080";
+pub const SERVER_URL: &str = "http://192.168.60.22:8080";
